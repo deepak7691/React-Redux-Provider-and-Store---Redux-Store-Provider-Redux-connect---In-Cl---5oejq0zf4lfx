874892d6a85ab89ed509034fd335021e87570b46
deepak7691 <kawliya11@gmail.com>
2023-03-30 23:13:13 +0530

    deepakk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import { render } from "react-dom";
 import { Provider } from "react-redux";
-import { store } from "redux";
+import {legacy_createStore as createStore } from "redux";
+import reducer from "./reducers/index";
+import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const store = createStore(reducer);
+render(
     <Provider store={store}>
         <App />
-    </Provider>
+    </Provider>,
+    document.getElementById("root")
 );
-
-
