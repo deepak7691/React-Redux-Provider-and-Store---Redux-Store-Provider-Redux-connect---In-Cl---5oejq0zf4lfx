39f53efea870ea2e18737246a376ce739a62ede4
deepak7691 <kawliya11@gmail.com>
2023-03-30 22:48:24 +0530

    deep

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import App from "./components/App";
 import { Provider } from "react-redux";
 import store from "./store";
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
 
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
